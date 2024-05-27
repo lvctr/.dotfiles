e7bec3b44535c6fcc5704905d218d130da2d40b1
alvi kazi <alvi@kazi.sh>
2024-05-27 18:16:30 +0900

    delete lavender

--- lavender/.config/polybar/scripts/fcitx.sh
+++
@@ -1,3 +0,0 @@
-PATH="/bin:/opt/polybar-fcitx-scripts"
-
-gdbus monitor --session --dest org.fcitx.Fcitx | grep --line-buffered / | react | sed -e 's/English (US)/EN/g;s/Mozc/JA/g'
--- lavender/.scripts/change_desktop.sh
+++
@@ -1,4 +0,0 @@
-xlayoutdisplay
-feh --bg-fill /home/insti/Pictures/Wallpaper/wallpaper.jpg
-/home/insti/.config/polybar/launch.sh &
-bspc wm -O eDP
